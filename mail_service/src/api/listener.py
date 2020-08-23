17789cc7007bfff4720cb3dd85a7675efb6b80dc
scumethy <scume7hy@gmail.com>
2020-08-23 20:28:48 +0300

    fix email settings

--- mail_service/src/api/listener.py
+++ mail_service/src/api/listener.py
@@ -15,11 +15,17 @@ async def on_message(message: IncomingMessage):
     em["Subject"] = "subject"
     em.set_content(message["text"])
 
-    await aiosmtplib.send(em, hostname="email", port=25)
+    await aiosmtplib.send(
+        em,
+        hostname="smtp.yandex.ru",
+        port=25,
+        username="scumethy@le-memese.com",
+        password="pfQsSs9iNM3UCAAT43MN82qxgJtuDtHjuevEr5et",
+        use_tls=True,
+    )
 
 
 async def listen(loop):
-    print(config.RABBITMQ_DSN)
     connection = await connect_robust(config.RABBITMQ_DSN, loop=loop)
     channel = await connection.channel()
     queue = await channel.declare_queue("mail")
